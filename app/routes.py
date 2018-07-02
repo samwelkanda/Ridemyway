efab2ed2b8f4e793a8db89f2490654b44bf04dda
Samwel Kanda <samwelkanda@gmail.com>
2018-07-02 20:51:00 +0300

    Added coditionals to the templates and configuration files

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/dashboard')
+def dashboard():
+    user = {'username': 'Kanda'}
+    rides = [
+        {
+            'driver': {'username': 'Paul Pogba'},
+            'time': '05/21/2018 09:00 AM',
+            'from': 'Karen',
+            'destination': 'Statehouse'
+        },
+        {
+            'driver': {'username': 'Marcus Rashford'},
+            'time': '05/21/2018 12:00 AM',
+            'from': 'JKIA',
+            'destination': 'Lavington'
+        },
+        {
+            'driver': {'username': 'Jesse Lingard'},
+            'time': '05/21/2018 05:00 PM',
+            'from': 'Upperhill',
+            'destination': 'Syokimau'
+        },
+
+    ]
+    return render_template('dashboard.html', title='Home', user=user, rides=rides)
